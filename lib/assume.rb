eecdafe34c124165f2d3f86d17fc75eb36430fd8
Odin H B <odinh2908@gmail.com>
2020-12-06 13:21:16 +0100

    isolate the internal state of the gem
    @@enabled and @@assumption_handler was available in every
    class where you included this gem
    
    I also don't like creating an extra method: handle_broken_assumptions
    which is available globally when Assume.handler= will do the trick
    and let you use procs/lambdas instead.

--- lib/assume.rb
+++ lib/assume.rb
@@ -3,22 +3,25 @@
 require "assume/version"
 
 module Assume
-  class BrokenAssumption < StandardError; end
-  @@assumption_handler = nil
-  @@enabled = false
+  class BadAssumption < StandardError; end
 
-  RAISE_HANDLER = lambda do |result, block|
+  DEFAULT_HANDLER = lambda do |result, block|
     filename, line = block.source_location
-    line_content = File.readlines(filename)[line - 1]
+    begin
+      lines = File.readlines(filename)
+      line_content = lines[line - 1]
+    rescue Errno::ENOENT => err
+      line_content = "<unable to open source code file>"
+    end
 
-    raise BrokenAssumption, "\nin #{filename}\nsource" \
+    raise BadAssumption, "\nin #{filename}\nsource" \
                             "code (line #{line}):\n#{line_content}\n" \
                             "result was: #{result.inspect}"
   end
 
   def assume(&block)
     raise ArgumentError, "assumptions need a block" if block.nil?
-    return unless @@enabled
+    return unless Assume.enabled?
 
     result = block.call
 
@@ -30,15 +33,23 @@ module Assume
   end
   alias assumption assume
 
-  def handle_broken_assumptions(&block)
-    @assumption_handler = block
-  end
+  class << self
+    def handler=(handler)
+      @assumption_handler = handler
+    end
 
-  def self.enable!
-    @@enabled = true
-  end
+    def enabled=(val)
+      @enabled = !!val
+    end
 
-  def self.broken_assumption_handler
-    @@assumption_handler || RAISE_HANDLER
+    def enabled?
+      @enabled
+    end
+
+    def broken_assumption_handler
+      @assumption_handler || DEFAULT_HANDLER
+    end
   end
 end
+
+Assume.enabled = false
--- spec/assume_spec.rb
+++ spec/assume_spec.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
-RSpec.describe Assume do
+RSpec.describe Assume, :aggregate_failures do
   it "has a version number" do
     expect(Assume::VERSION).not_to be nil
   end
 
-  it "is not available by default", :aggregate_failures do
+  it "is not available by default" do
     expect { assume { true } }
       .to raise_error(NoMethodError)
     expect { assumption { true } }
-      .to raise_error(NoMethodError)
-    expect { handle_broken_assumptions {} }
       .to raise_error(NoMethodError)
   end
 
@@ -34,9 +32,9 @@ RSpec.describe Assume do
         end
       end
 
-      describe "when enabled", :aggregate_failures do
+      describe "when enabled" do
         before(:each) do
-          Assume.enable!
+          Assume.enabled = true
         end
 
         it "actually runs the block" do
@@ -47,17 +45,39 @@ RSpec.describe Assume do
         it "does nothing and returns nil if the condition is true" do
           expect(assume { true }).to be nil
           expect(assumption { true }).to be nil
+          expect(assumption { 5 > 2 }).to be nil
         end
 
-        it "raises BrokenAssumption if the condition is false" do
+        it "raises BadAssumption if the condition is false" do
           expect { assume { false } }
-            .to raise_error(Assume::BrokenAssumption)
+            .to raise_error(Assume::BadAssumption)
           expect { assumption { false } }
-            .to raise_error(Assume::BrokenAssumption)
+            .to raise_error(Assume::BadAssumption)
           expect { assume { true == false } }
-            .to raise_error(Assume::BrokenAssumption)
+            .to raise_error(Assume::BadAssumption)
           expect { assumption { "yes" == "no" } }
-            .to raise_error(Assume::BrokenAssumption)
+            .to raise_error(Assume::BadAssumption)
+        end
+      end
+
+      describe "with a custom handler" do
+        it "runs the block set as handler" do
+          bad = 0
+          Assume.handler = proc { bad += 1 }
+
+          assume { false }
+          expect(bad).to eq(1)
+          assumption { false }
+          expect(bad).to eq(2)
+
+          Assume.handler = proc { |result, block|
+            expect(result).to be false
+            expect(block).to be_a(Proc)
+            bad += 1
+          }
+
+          assume { false }
+          expect(bad).to eq(3)
         end
       end
     end
